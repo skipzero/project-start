6703146461c7aea037c0d385adac9501681b4d37
Jinx <Rapture@darkAngel.local>
2016-10-02 17:53:57 -0700

    init commit stripped down

---
+++ README.md
@@ -0,0 +1,24 @@
+# Noughts and Crosses
+Noughts and Crosses (tic tac toe) with react.js
+
+## About this game
+
+generic repo with webpack, eslint, babel n stuff to quickly start new projects without dicking around.
+
+### Getting Started
+
+clone repo
+
+    npm install
+
+**Watch files:**
+
+to watch files run
+
+    npm run dev
+
+**To start server:**
+
+to run locally
+
+    npm start
---
+++ src/index.js
@@ -0,0 +1,17 @@
+/*************************************/
+/*  Uncomment the react lines as needed...
+/*  This is our entry point for the main app.
+/*  We require our other modules here to create our bundle.
+/*  Currently set up for react but we can do a node app.
+/*  just add a server.js file and get it set up in there.
+/*
+/************************************/
+
+// require('./stylesheets/main.scss');
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// import Game from './javascripts/board.jsx';
+// import Store from './javascripts/store.js';
+
+// ReactDOM.render(<Game store={window.store} />, document.getElementById('game'));
